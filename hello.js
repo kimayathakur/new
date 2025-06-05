81eb8c226d96180dc7e7f19126d53e12fb1bc67e
kimayathakur <thakurkimaya21@gmail.com>
2025-06-05 10:33:13 +0530

    i learned function

--- hello.js
+++ hello.js
@@ -1,28 +1,10 @@
-console.log("hello world");
-
-let val=19;
-let val2=20;
-val="hello";
-val=false
-console.log(val)
-const pi = 3.14
-console.log(pi)
-let fil=75;
-if(fil<=50)
-{
-    console.log("fill is above 50")
-}else if(fil<=60)
-{
-    console.log("fill is above 60")
-}
-else {
-    console.log("fill is above 75")
-}
-if(fil%2==0)
-{
-    console.log("even")
-}
-if(fil%2!=0)
-{
-    console.log("odd")
-}
+// const prompt = require('prompt-sync')();
+// const name = prompt("What is your name? ");
+// console.log("Hello, " + name + "!");
+ function square(x)
+ {
+    let ans=0;
+    ans=x*x;
+    return ans;
+ }
+ console.log ("the answer is:"+square(9));
