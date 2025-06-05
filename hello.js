b7dfb741bd2923b50857e791a388660a36d1e642
kimayathakur <thakurkimaya21@gmail.com>
2025-06-05 09:15:37 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+let val=19;
+val="hello";
+val=false
+console.log(val)
+const pi = 3.14
+console.log(pi)
+let fil=75;
+if(fil<=50)
+{
+    console.log("fill is above 50")
+}else if(fil<=60)
+{
+    console.log("fill is above 60")
+}
+else {
+    console.log("fill is above 75")
+}
+if(fil%2==0)
+{
+    console.log("even")
+}
+if(fil%2!=0)
+{
+    console.log("odd")
+}
