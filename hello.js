b792ffbc0406849bd4238a95dd0757c8ee01236f
kimayathakur <thakurkimaya21@gmail.com>
2025-06-05 11:16:58 +0530

    implement callback

--- hello.js
+++ hello.js
@@ -1,10 +1,21 @@
 // const prompt = require('prompt-sync')();
 // const name = prompt("What is your name? ");
 // console.log("Hello, " + name + "!");
- function square(x)
+//  function square(x)
+//  {
+//     let ans=0;
+//     ans=x*x;
+//     return ans;
+//  }
+//  console.log ("the answer is:"+square(9));
+ function greet(name,ca)
  {
-    let ans=0;
-    ans=x*x;
-    return ans;
+    console.log("hello,"+name + "!");
+    ca();
+
  }
- console.log ("the answer is:"+square(9));
+ function g()
+ {
+    console.log("goodbye!");
+ }
+ greet("kimaya",g);
